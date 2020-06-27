7f1052b86aee382cb396cf9b7c40eddc74abe33f
Arafat Husayn <hello@arafat.dev>
2020-06-27 21:57:27 +0600

    add tests for string answers with 100% coverage

--- test/main.test.ts
+++ test/main.test.ts
@@ -14,7 +14,20 @@ describe("spAnswer", () => {
       },
     });
 
-    expect(result && result.positive).toBe(false);
+    expect(result && result.answer).toBe(false);
+  });
+
+  test(`Supports string answers and returns answer === "no" for question: '${questionText}'`, () => {
+    const answers = getStringAnswersForPhiladelphiaQuestion();
+
+    const result = spAnswer({
+      answers,
+      question: {
+        text: questionText,
+      },
+    });
+
+    expect(result && result.answer).toStrictEqual("no");
   });
 
   questionText =
@@ -39,10 +52,10 @@ describe("spAnswer", () => {
       },
     });
 
-    expect(result && result.positive).toBe(true);
+    expect(result && result.answer).toBe(true);
   });
 
-  test(`Returns null if the given answers are not having boolean types for postive & popular`, () => {
+  test(`Returns null if the given answers are not having boolean/string types for postive & popular`, () => {
     let result = spAnswer({
       answers: [
         {
@@ -72,7 +85,7 @@ describe("spAnswer", () => {
     expect(result).toBe(null);
   });
 
-  test(`threshold is applied if provided`, () => {
+  test(`Threshold is applied if provided`, () => {
     const answers = getAnswersForPhiladelphiaQuestion();
 
     const result = spAnswer({
@@ -83,15 +96,80 @@ describe("spAnswer", () => {
       threshold: -21,
     });
 
-    expect(result && result.positive).toBe(true);
+    expect(result && result.answer).toBe(true);
   });
 
-  test(`Return empty string for question.text if question is not provided in args`, () => {
+  test(`Returns empty string for question.text if question is not provided in args`, () => {
     const result = spAnswer({
       answers: [{ positive: true, popular: true }],
     });
 
     expect(result && result.question && result.question.text).toBe("");
+  });
+
+  test(`Returns null if one of the answers is an empty string`, () => {
+    let result = spAnswer({
+      answers: [
+        {
+          positive: "",
+          popular: "test",
+        },
+      ],
+      question: {
+        text: questionText,
+      },
+    });
+
+    expect(result).toBe(null);
+
+    result = spAnswer({
+      answers: [
+        {
+          positive: "test",
+          popular: "",
+        },
+      ],
+      question: {
+        text: questionText,
+      },
+    });
+
+    expect(result).toBe(null);
+  });
+
+  test(`Returns string[] for answer if there are multiple answers with the same score.`, () => {
+    let answers = [];
+    const options = ["heads", "tails"];
+
+    for (let i = 0; i < 100; i++) {
+      let answer: Answer = {
+        positive: "",
+        popular: "",
+      };
+
+      if (i < 50) {
+        answer.positive = options[0];
+      } else {
+        answer.positive = options[1];
+      }
+
+      if (i < 50) {
+        answer.popular = options[0];
+      } else {
+        answer.popular = options[1];
+      }
+
+      answers.push(answer);
+    }
+
+    const result = spAnswer({
+      answers,
+      question: {
+        text: "Heads or Tails?",
+      },
+    });
+
+    expect(result && result.answer).toStrictEqual(options);
   });
 });
 
@@ -114,6 +192,33 @@ function getAnswersForPhiladelphiaQuestion(): Answer[] {
       answer.popular = true;
     } else {
       answer.popular = false;
+    }
+
+    answers.push(answer);
+  }
+
+  return answers;
+}
+
+function getStringAnswersForPhiladelphiaQuestion(): Answer[] {
+  let answers: Answer[] = [];
+
+  for (let i = 0; i < 100; i++) {
+    let answer: Answer = {
+      positive: "yes",
+      popular: "yes",
+    };
+
+    if (i < 65) {
+      answer.positive = "yes";
+    } else {
+      answer.positive = "no";
+    }
+
+    if (i < 75) {
+      answer.popular = "yes";
+    } else {
+      answer.popular = "no";
     }
 
     answers.push(answer);
